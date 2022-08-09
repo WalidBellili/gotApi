8802e53a0deae9009b26b6b441f0332ae2184c95
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-08-09 10:27:50 +0200

    axios for pokemons

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const port = 5001;
 const axios = require("axios");
 const gotJson = require("./got.json");
 const gotRoute = require("./routes/gotRoute");
+const pokemons = require("./routes/pokemons");
 
 app.use("/gameOfThrone", gotRoute);
+app.use("/pokemons", pokemons);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
---
+++ routes/pokemons.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+app.get("/api", async (req, res) => {
+  const response = await axios.get(
+    "https://pokeapi.co/api/v2/pokemon/?limit=151"
+  );
+
+  res.send(response.data);
+});
+
+module.exports = app;
