1701347b66abfe464f87f932ea548fe1b9dbfc03
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-08-09 10:12:20 +0200

    axios for got

--- routes/gotRoute.js
+++ routes/gotRoute.js
@@ -3,6 +3,10 @@ const app = express();
 const gotJson = require("../got.json");
 
 app.get("/json", (req, res) => {
+  const getData = async () => {
+    const response = await axios.get("http://localhost:5001/gameOfThrone/json");
+    console.log(response.data);
+  };
   res.send(gotJson);
 });
 module.exports = app;
