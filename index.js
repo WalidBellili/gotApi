417d2c9d62e6d10e924ffcfd3baf6cd52be0e2d6
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-08-09 10:09:00 +0200

    first exo

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = 5001;
+const axios = require("axios");
 const gotJson = require("./got.json");
+const gotRoute = require("./routes/gotRoute");
 
-app.use("/json", gotJson);
-
-
-app.get("/", (req, res) => {
-  res.send(gotJson);
-});
+app.use("/gameOfThrone", gotRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- routes/gotRoute.js
+++ routes/gotRoute.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
-const gotJson = require("./got.json");
+const gotJson = require("../got.json");
 
+app.get("/json", (req, res) => {
+  res.send(gotJson);
+});
 module.exports = app;
